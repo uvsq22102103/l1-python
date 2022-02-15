89735d60f6ec44ba8a4d02be6530cb8e0e72a31b
Aymeric GOUDOUT <aymeric.goudout@ens.uvsq.fr>
2022-02-15 22:17:45 +0100

    collab tas de sable V0.3

--- bi202/collab_tas_de_sable.py
+++ bi202/collab_tas_de_sable.py
@@ -12,6 +12,7 @@
 
 import tkinter as tk
 import random as rd
+import time
 
 ##########################
 #Définition des constantes
@@ -19,7 +20,7 @@ import random as rd
 
 HAUTEUR = 800
 LARGEUR = 800
-GRILLE = 10
+GRILLE = 20
 
 ##################################
 #Définition des variables globales
@@ -27,16 +28,18 @@ GRILLE = 10
 
 liste_canvas = []
 config_courante = []
+old_config = []
 
 #########################
 #Définition des fonctions
 #########################
 
-def carre_canvas(x,y,rayon=5,couleur='blue'):
-    liste_canvas = canvas.create_rectangle(x-rayon,y-rayon,x+rayon,y+rayon,fill=couleur)
+def oval_canvas(x,y,rayon=7,couleur='blue'):
+    liste_canvas = canvas.create_oval(x-rayon,y-rayon,x+rayon,y+rayon,fill=couleur)
 
 def generation():
     canvas.delete('all')
+    global config_courante
     config_courante = []
     for i in range(GRILLE):
         config_courante.append([])
@@ -46,9 +49,33 @@ def generation():
     for x in range(GRILLE):
         for y in range(GRILLE):
             if config_courante[x][y] < 4:
-                carre_canvas(espacement / 2 + x * espacement, espacement / 2 + y*espacement)
+                oval_canvas(espacement / 2 + x * espacement, espacement / 2 + y*espacement)
             else:
-                carre_canvas(espacement / 2 + x * espacement, espacement / 2 + y*espacement,couleur='red')
+                oval_canvas(espacement / 2 + x * espacement, espacement / 2 + y*espacement,couleur='red')
+
+def avalanche():
+    #Cycle + actualisation while Cycle est effectif. 
+    pass
+
+def actualisation():
+    global config_courante
+    #Permet de mettre à jour l'affichage des grains de sable.
+    pass
+
+def cycle():
+    global config_courante,old_config
+    #Un calcul sur la matrice qui suis le raisonnement d'un tas de sable.
+    old_config = config_courante
+
+    pass
+
+def affichage_valeurs():
+    global config_courante
+    espacement = HAUTEUR / GRILLE
+    for x in range(GRILLE):
+        for y in range(GRILLE):
+            canvas.create_text(espacement / 2 + x * espacement, espacement / 2 + y*espacement,fill='white',text=config_courante[x][y])
+            
 
 ####################
 #Programme principal
@@ -58,7 +85,12 @@ root = tk.Tk()
 root.title('Tas de Sable - Groupe 1')
 canvas = tk.Canvas(root,height=HAUTEUR,width=LARGEUR,bg='black')
 button_generation = tk.Button(root,text='Génération',command=generation,activebackground='red')
+button_avalanche = tk.Button(root,text='Avalanche',command=avalanche)
+button_affichage_valeurs = tk.Button(root,text='Valeurs',command=affichage_valeurs)
+
 button_generation.grid(row=0,column=0)
-canvas.grid(row=0,column=1)
+button_avalanche.grid(row=1,column=0)
+button_affichage_valeurs.grid(row=2,column=0)
+canvas.grid(row=0,rowspan=3,column=1)
 
 root.mainloop()
