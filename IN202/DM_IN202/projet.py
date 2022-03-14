ba6eba698725a824a7fdf4275d2178aa68702d39
Aymeric GOUDOUT <aymeric.goudout@ens.uvsq.fr>
2022-03-14 08:47:04 +0100

    qr code advancement

--- IN202/DM_IN202/projet.py
+++ IN202/DM_IN202/projet.py
@@ -1,7 +1,12 @@
 import PIL as pil
 from PIL import Image
-from PIL import ImageTk 
+from PIL import ImageTk
 
+def nbrCol(matrice):
+    return(len(matrice[0]))
+
+def nbrLig(matrice):
+    return len(matrice)
 
 def saving(matPix, filename):#sauvegarde l'image contenue dans matpix dans le fichier filename
 							 #utiliser une extension png pour que la fonction fonctionne sans perte d'information
@@ -19,3 +24,32 @@ def loading(filename):#charge le fichier image filename et renvoie une matrice d
         for j in range(toLoad.size[0]):
             mat[i][j]= 0 if toLoad.getpixel((j,i)) == 0 else 1
     return mat
+
+def affiche_mat(matrice):
+    for i in matrice:
+        print(i)
+
+def qr_coin():
+    coin = []
+    for i in range(9):
+        coin.append([])
+        for j in range(9):
+            coin[i].append(0)
+    for i in range(7):
+        i += 1
+        for j in range(7):
+            j += 1
+            coin[i][j] = 1
+    for i in range(5):
+        i += 2
+        for j in range(5):
+            j += 2
+            coin[i][j] = 0
+    for i in range(3):
+        i += 3
+        for j in range(3):
+            j += 3
+            coin[i][j] = 1
+    return coin
+
+affiche_mat(qr_coin())
