416e1547f0de3c12b2563bf094bca10a6d1e717c
Aymeric GOUDOUT <aymeric.goudout@ens.uvsq.fr>
2022-03-20 16:09:45 +0100

    HAMM

--- IN202 et 200/DM_IN202/hamming.py
+++ IN202 et 200/DM_IN202/hamming.py
@@ -11,28 +11,34 @@ def hamming(bits:list):
 
 
 def reverse_hamming(bits:list):
-    c1, c2, c3 = parité(bits,[0,1,4]), parité(bits,[0,2,4]), parité(bits,[2,1,4])
+    c1, c2, c3 = parité(bits,[2,4,6]), parité(bits,[2,5,6]), parité(bits,[4,5,6])
     k1, k2, k3 = bits[0], bits[1], bits[3]
-    if c1 != k1 and c2 != k2 and c3 == k3:
-        bits[0] = 0 if bits[0] == 1 else 1
-        return [bits[0], bits[1], bits[2], bits[4]]
-    elif c1 != k1 and c2 == k2 and c3 != k3:
-        bits[1] = 0 if bits[1] == 1 else 1
-        return [bits[0], bits[1], bits[2], bits[4]]
+    if c1 == k1 and c2 == k2 and c3 == k3:
+        return [bits[2],bits[4],bits[5],bits[6]]
+    elif c1 != k1 and c2 != k2 and c3 == k3:
+        bits[2] = 0 if bits[2] == 1 else 0
+        return [bits[2],bits[4],bits[5],bits[6]]
     elif c1 == k1 and c2 != k2 and c3 != k3:
-        bits[2] = 0 if bits[2] == 1 else 1
-        return [bits[0], bits[1], bits[2], bits[4]]
+        bits[5] = 0 if bits[5] == 1 else 0
+        return [bits[2],bits[4],bits[5],bits[6]]
+    elif c1 != k1 and c2 == k2 and c3 != k3:
+        bits[4] = 0 if bits[4] == 1 else 0
+        return [bits[2],bits[4],bits[5],bits[6]]
     elif c1 != k1 and c2 != k2 and c3 != k3:
-        bits[4] = 0 if bits[4] == 1 else 1
-        return [bits[0], bits[1], bits[2], bits[4]]
-    elif c1 == k1 and c2 == k2 and c3 == k3:
-        return [bits[0], bits[1], bits[2], bits[4]]
+        bits[6] = 0 if bits[6] == 1 else 0
+        return [bits[2],bits[4],bits[5],bits[6]]
     elif c1 != k1 and c2 == k2 and c3 == k3:
-        return [bits[0], bits[1], bits[2], bits[4]]
+        return [bits[2],bits[4],bits[5],bits[6]]
     elif c1 == k1 and c2 != k2 and c3 == k3:
-        return [bits[0], bits[1], bits[2], bits[4]]
+        return [bits[2],bits[4],bits[5],bits[6]]
     elif c1 == k1 and c2 == k2 and c3 != k3:
-        return [bits[0], bits[1], bits[2], bits[4]]
+        return [bits[2],bits[4],bits[5],bits[6]]
 
-print(hamming([1,1,0,0]))
-print(reverse_hamming([0,1,1,1,1,0,0]))
+
+test = hamming([1, 1, 0, 0])
+print(test)
+tests = [[1, 1, 1, 1, 1, 0, 0], [0, 0, 1, 1, 1, 0, 0], [0, 1, 0, 1, 1, 0, 0], [0, 1, 1, 0, 1, 0, 0], [0, 1, 1, 1, 0, 0, 0], [0, 1, 1, 1, 1, 1, 0], [0, 1, 1, 1, 1, 0, 1]]
+print(reverse_hamming(test))
+print('\n')
+for i in tests:
+    print(reverse_hamming(i))
