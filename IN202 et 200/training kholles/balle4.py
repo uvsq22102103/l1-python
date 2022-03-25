e9086c8192abd299fa5f75c6e1ee1ce5362fb17e
Aymeric GOUDOUT <aymeric.goudout@ens.uvsq.fr>
2022-03-25 07:15:44 +0100

    balle 4

--- IN202 et 200/training kholles/balle4.py
+++ IN202 et 200/training kholles/balle4.py
@@ -5,6 +5,8 @@ import tkinter as tk
 
 LARGEUR = 600
 HAUTEUR = 400
+compteur = 0
+compteur_change_balle = 0
 
 
 ###################
@@ -24,19 +26,37 @@ def creer_balle():
 
 def mouvement():
     """Déplace la balle et ré-appelle la fonction avec un compte-à-rebours"""
-    rebond()
-    canvas.move(balle[0], balle[1], balle[2])
-    canvas.after(20, mouvement)
+    if compteur < 30:
+        rebond()
+        canvas.move(balle[0], balle[1], balle[2])
+        canvas.after(20, mouvement)
+
+
+def change_balle():
+    global compteur_change_balle
+    if compteur % 5 == 0 and compteur != 0:
+        balle_coords = canvas.coords(balle[0])
+        canvas.delete(balle[0])
+        if compteur_change_balle % 2 == 0:
+            balle[0] = canvas.create_rectangle(balle_coords[0],balle_coords[1],balle_coords[2],balle_coords[3],fill='green')
+        elif compteur_change_balle % 2 != 0:
+            balle[0] = canvas.create_oval(balle_coords[0],balle_coords[1],balle_coords[2],balle_coords[3],fill='blue')
+        compteur_change_balle += 1
+
 
 
 def rebond():
     """Fait rebondir la balle sur les bords du canevas"""
-    global balle
+    global balle, compteur
     x0, y0, x1, y1 = canvas.coords(balle[0])
     if x0 <= 0 or x1 >= 600:
         balle[1] = -balle[1]
+        compteur += 1
+        change_balle()
     if y0 <= 0 or y1 >= 400:
         balle[2] = -balle[2]
+        compteur += 1
+        change_balle()
 
 
 ######################
