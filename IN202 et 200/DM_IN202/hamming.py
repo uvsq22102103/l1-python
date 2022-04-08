eba81a4764e68db91ab1f079cc80277e95312ca1
Aymeric GOUDOUT <aymeric.goudout@ens.uvsq.fr>
2022-04-08 12:13:22 +0200

    backup si pb

--- IN202 et 200/DM_IN202/hamming.py
+++ IN202 et 200/DM_IN202/hamming.py
@@ -5,40 +5,27 @@ def parité(bits:list, indices:list):
     return out % 2
 
 
-def hamming(bits:list):
-    c1, c2, c3 = parité(bits,[0,1,3]), parité(bits,[0,2,3]), parité(bits,[2,1,3])
-    return [c1,c2,bits[0],c3,bits[1],bits[2],bits[3]]
+def hamming(bits: list):
+    c1, c2, c3 = parité(bits, [0, 1, 3]), parité(bits, [0, 2, 3]), parité(bits, [2, 1, 3])
+    return [bits[0], bits[1], bits[2], bits[3], c1, c2, c3]
 
 
-def reverse_hamming(bits:list):
-    c1, c2, c3 = parité(bits,[2,4,6]), parité(bits,[2,5,6]), parité(bits,[4,5,6])
-    k1, k2, k3 = bits[0], bits[1], bits[3]
-    if c1 == k1 and c2 == k2 and c3 == k3:
-        return [bits[2],bits[4],bits[5],bits[6]]
-    elif c1 != k1 and c2 != k2 and c3 == k3:
-        bits[2] = 0 if bits[2] == 1 else 0
-        return [bits[2],bits[4],bits[5],bits[6]]
-    elif c1 == k1 and c2 != k2 and c3 != k3:
-        bits[5] = 0 if bits[5] == 1 else 0
-        return [bits[2],bits[4],bits[5],bits[6]]
-    elif c1 != k1 and c2 == k2 and c3 != k3:
-        bits[4] = 0 if bits[4] == 1 else 0
-        return [bits[2],bits[4],bits[5],bits[6]]
-    elif c1 != k1 and c2 != k2 and c3 != k3:
-        bits[6] = 0 if bits[6] == 1 else 0
-        return [bits[2],bits[4],bits[5],bits[6]]
-    elif c1 != k1 and c2 == k2 and c3 == k3:
-        return [bits[2],bits[4],bits[5],bits[6]]
-    elif c1 == k1 and c2 != k2 and c3 == k3:
-        return [bits[2],bits[4],bits[5],bits[6]]
-    elif c1 == k1 and c2 == k2 and c3 != k3:
-        return [bits[2],bits[4],bits[5],bits[6]]
+def switch_bin(binary: int):
+    if binary == 0:
+        binary = 1
+    else:
+        binary = 0
+    return binary
 
 
-test = hamming([1, 1, 0, 0])
-print(test)
-tests = [[1, 1, 1, 1, 1, 0, 0], [0, 0, 1, 1, 1, 0, 0], [0, 1, 0, 1, 1, 0, 0], [0, 1, 1, 0, 1, 0, 0], [0, 1, 1, 1, 0, 0, 0], [0, 1, 1, 1, 1, 1, 0], [0, 1, 1, 1, 1, 0, 1]]
-print(reverse_hamming(test))
-print('\n')
-for i in tests:
-    print(reverse_hamming(i))
+def reverse_hamming(bits: list):
+    c1, c2, c3 = parité(bits, [0, 1, 3]), parité(bits, [0, 2, 3]), parité(bits, [2, 1, 3])
+    if c1 != bits[-3] and c2 != bits[-2] and c3 != bits[-1]:
+        bits[3] = switch_bin(bits[3])
+    elif c2 != bits[-2] and c3 != bits[-1]:
+        bits[2] = switch_bin(bits[2])
+    elif c1 != bits[-3] and c3 != bits[-1]:
+        bits[1] = switch_bin(bits[1])
+    elif c1 != bits[-3] and c2 != bits[-2]:
+        bits[0] = switch_bin(bits[0])
+    return [bits[0], bits[1], bits[2], bits[3]]
--- IN202 et 200/test.py
+++ IN202 et 200/test.py
@@ -1,21 +1,5 @@
-import tkinter as tk
-import time
-
-def comptage():
-    compteur = 0
-    for i in range(200):
-        compteur += 1
-        texte.set(compteur)
-        root.update()
-        time.sleep(1)
-
-root = tk.Tk()
-texte = tk.IntVar()
-
-boutton_compteur = tk.Button(root,text='Compteur',command=comptage)
-label_dynamique = tk.Label(root,textvariable=texte)
-
-boutton_compteur.grid(row=0,column=0)
-label_dynamique.grid(row=0,column=1)
-
-root.mainloop()
+s = "Hello"
+l = list(s)
+l[0] = "Z"
+s = "".join(l)
+print(s)
